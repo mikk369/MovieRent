251904f50f6b302f0a9caf92b186c728881ca49f
mikk369 <mikk.ereline@vikk.ee>
2022-05-29 18:09:38 +0300

    added user Routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+router.param('id', (req, res, next, val) => {
+  console.log(`User id is ${val}`);
+  next();
+});
+
+router
+  .route('/')
+  .get(userController.getAllusers)
+  .post(userController.createUser);
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
+
+module.exports = router;
