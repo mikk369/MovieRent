b2743fbae409264277ec8137bc32ae11142cf640
mikk369 <mikk.ereline@vikk.ee>
2022-05-29 18:05:27 +0300

    created user controllers

---
+++ controllers/userController.js
@@ -0,0 +1,30 @@
+exports.getAllusers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'this route not working yet',
+  });
+};
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'this route not working yet',
+  });
+};
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'this route not working yet',
+  });
+};
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'this route not working yet',
+  });
+};
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'this route not working yet',
+  });
+};
