130aba37c6e14a912adcd34dce7132f001faed4c
mikk369 <mikk.ereline@vikk.ee>
2022-05-29 18:02:32 +0300

    created  import DB and delete DB script

---
+++ import-dev-data.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Movie = require('./models/movieModel');
+
+dotenv.config({ path: './config.env' });
+
+//gets the password from config.env and replaces it in database adress
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+//connects database with mongoose
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  });
+
+//reading JS FILE
+const movies = JSON.parse(
+  fs.readFileSync(`${__dirname}/movie_list/moviesData.json`, 'utf-8')
+);
+
+//IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await Movie.create(movies);
+    console.log('Data successfully loadaded');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+//DELETE ALL DATA FROM DB
+const deleteData = async () => {
+  try {
+    await Movie.deleteMany();
+    console.log('Data successfully deleted');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+console.log(process.argv);
