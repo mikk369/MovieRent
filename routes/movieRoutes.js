c5776436a68943a78d6df1799a5461357e09a214
mikk369 <mikk.ereline@vikk.ee>
2022-06-02 16:55:09 +0300

    added new routes oldest,newest,statistic

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -7,6 +7,14 @@ const movieController = require('../controllers/movieController');
 // router.param('id', movieController.chekID);
 
 router
+  .route('/top-10-cheap')
+  .get(movieController.aliasTopMovies, movieController.getAllMovies);
+
+router.route('/movie-stats').get(movieController.getMovieStats);
+router.route('/newest-first').get(movieController.newestFirst);
+router.route('/oldest-first').get(movieController.oldestFirst);
+
+router
   .route('/')
   .get(movieController.getAllMovies)
   .post(movieController.createMovie);
