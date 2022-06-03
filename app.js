bd574a1f38c6b76ac038dad82798da0a5691469d
mikk369 <mikk.ereline@vikk.ee>
2022-06-03 19:35:16 +0300

    added better error handling

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const movieRouter = require('./routes/movieRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -13,11 +15,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware!');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -25,5 +22,12 @@ app.use((req, res, next) => {
 //MOUNTING ROUTES
 app.use('/api/movies', movieRouter);
 app.use('/api/users', userRouter);
+
+//Error handling when any other routes from UP ↑ didnt send response and it got to this point here:
+app.all('*', (req, res, next) => {
+  next(new AppError(`Cant find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
