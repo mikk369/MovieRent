ad5e05bf0f8efedde01076c4e9f3dd94386d9cf8
mikk369 <mikk.ereline@vikk.ee>
2022-06-09 21:34:57 +0300

    Changed

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,68 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFields = (err) => {
+  //loops over error objects
+
+  const value = err.errmsg.mact(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 404);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorprod = (err, req) => {
+  //operational, trusted error: send message to client
+  if (err.isOpperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    //programming or other unknown error: dont want to send details to cliend
+  } else {
+    //1) log to console
+    console.log('ERROR !!!', err);
+    //2) send generic mssage
+    res.status(500).json({
+      status: 'Error',
+      message: 'Something went wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   // err.statusCode is equal to err.statusCode if its defined if not its 500(eternal server error)
   err.statusCode = err.statusCode || 500;
   //err.status is equal to err.status if its defined if not its 'error'
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    //handles cast error when the movie id is wrong
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    //handles title error when movie title is already entered
+    if (error.code === 11000) error = handleDuplicateFields(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+    sendErrorprod(error, res);
+  }
 };
