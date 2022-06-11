4664de361e58a9c679d2cc4ee2fc81c13503addd
mikk369 <mikk.ereline@vikk.ee>
2022-06-11 22:45:00 +0300

    added password bcrypt.compare

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -8,17 +9,27 @@ const userSchema = new mongoose.Schema({
     unique: [true, 'Name already used'],
     trim: true,
     maxlength: [15, 'User name must have less or equal 1 characters'],
-    minlenght: [1, 'User name must have atleast 1 character'],
+    minlength: [1, 'User name must have atleast 1 character'],
   },
+
   password: {
     type: String,
     required: [true, 'Please provide password'],
-    minlenght: 8,
+    minlength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      //if el(password confirm) is equal to password returns: true(password === passwordConfirm). THIS keyword works only on User.CREATE or .SAVE in authController, cant use THIS keyworkd with .FINDONE .UPDATE/ONE
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same',
+    },
   },
+
   email: {
     type: String,
     required: [true, 'Please provide email'],
@@ -34,7 +45,29 @@ const userSchema = new mongoose.Schema({
     defaul: Date.now(),
   },
 });
+
+userSchema.pre('save', async function (next) {
+  //if password is modified then run the following code
+  if (!this.isModified('password')) return next();
+  //hash current password,12 means how long is the random string(salt)
+  // this.password = await bcrypt.hash(this.password, 12);
+  const salt = await bcrypt.genSalt(12);
+  const hashedPassword = await bcrypt.hash(this.password, salt);
+  this.password = hashedPassword;
+  next();
+  //to delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+//calling function correctPassword that accepts candidatePassword that user passes in body.
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  //since password select is set to false so need to pass in userPassword also with compare function
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
 //user MODEL
 const User = mongoose.model('User', userSchema);
-
 module.exports = User;
