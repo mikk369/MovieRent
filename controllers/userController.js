e6d04409d4c4408657f979a3838dc4130a20d854
mikk369 <mikk.ereline@vikk.ee>
2022-06-11 22:43:41 +0300

    added get all users route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,18 @@
-exports.getAllusers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route not working yet',
+const catchAsync = require('./../utils/catchAsync');
+const User = require('../models/userModel');
+
+exports.getAllusers = catchAsync(async (req, res) => {
+  const users = await User.find();
+
+  //send response
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
   });
-};
+});
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
