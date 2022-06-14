8f3b3a55901f6072442a526f23bde7fb72c0b97e
mikk369 <mikk.ereline@vikk.ee>
2022-06-14 17:30:00 +0300

    protected getall movie route with login auth token

--- controllers/authController.js
+++ controllers/authController.js
@@ -54,3 +54,30 @@ exports.login = catchAsync(async (req, res, next) => {
     token,
   });
 });
+// TODO:not working
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check of it's there
+  let token;
+  if (
+    //if header excists and it starts with Bearer
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    //then token is equal to
+    token = req.headers.authorization.split(' ')[1];
+  }
+  console.log(token);
+  //if there is no token then return error
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access.', 401)
+    );
+  }
+  // 2) verification token
+
+  // 3) check if user still exists
+
+  // 4) check if user changed password after the token was issued
+
+  next();
+});
