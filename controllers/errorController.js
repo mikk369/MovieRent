04950cf9194a21d6f00c974f12a355ddb8e7a2c2
mikk369 <mikk.ereline@vikk.ee>
2022-06-15 17:56:13 +0300

    added tokenExpiredError

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -19,6 +19,12 @@ const handleValidationErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleJWTError = () =>
+  new AppError('Invalid token. Please lofin again!', 401);
+
+const handleJWTExpiredError = () =>
+  new AppError('Your token has been expired, please log in agaen !', 401);
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -57,12 +63,15 @@ module.exports = (err, req, res, next) => {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
+
     //handles cast error when the movie id is wrong
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     //handles title error when movie title is already entered
     if (error.code === 11000) error = handleDuplicateFields(error);
     if (error.name === 'ValidationError')
       error = handleValidationErrorDB(error);
+    if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
     sendErrorprod(error, res);
   }
 };
