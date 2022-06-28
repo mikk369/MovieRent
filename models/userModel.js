1893d83e35ab583d3b49570eb0dcb93259e9a5f1
mikk369 <mikk.ereline@vikk.ee>
2022-06-28 23:50:21 +0300

    added passwordReset and passwordExpires token

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('/crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -36,6 +37,8 @@ const userSchema = new mongoose.Schema({
     },
   },
   passwordChangedAt: Date,
+  createPasswordResetToken: String,
+  passwordResetExpires: Date,
   email: {
     type: String,
     required: [true, 'Please provide email'],
@@ -87,6 +90,15 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
 
   // False means NOT changed
   return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
 };
 
 //user MODEL
