f0e25f17fa24d946e2cbf58bd57185b345facdf4
mikk369 <mikk.ereline@vikk.ee>
2022-07-25 21:20:22 +0300

    transporter

--- utils/email.js
+++ utils/email.js
@@ -1,15 +1,26 @@
 const nodemailer = require('nodemailer');
 
-const sendEmail = (options) => {
+const sendEmail = async (options) => {
   // 1) create transporter
   const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
     },
-    //Activate in gmail "Less secure app" option
   });
   //2) define the email options
+
+  const mailOptions = {
+    from: 'Mikk E. <mikk12@gmail.com>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    // html:
+  };
   // 3) actually send the email
+  await transporter.sendMail(mailOptions);
 };
+
+module.exports = sendEmail;
